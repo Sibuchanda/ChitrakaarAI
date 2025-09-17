5e0a8d826eb557c724359ac5a3e118992a9127f4
Sibuchanda <sibuchanda457@gmail.com>
2025-09-17 20:16:18 +0530

    Edit configurtion in backend for Production

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,12 @@ const PORT = process.env.PORT || 4000
 const app = express();
 
 app.use(express.json());
-app.use(cors("*"));
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods:"GET,POST,PUT,DELETE",
+    allowedHeaders: ["Content-Type", "Authorization"]
+}))
 
 await connectDB();
 
